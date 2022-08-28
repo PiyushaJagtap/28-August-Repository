d7c273b3ae4ce118ea9afd2930b1fafe2e47ced9
Piyusha <jagtappiyusha31@gmail.com>
2022-08-28 12:19:49 +0530

    Team Lead Project Feature
    This is the staggng area .. commit it and make it local repo

---
+++ teamLeadProject/src/teamLeadProjectPKG/LeadClass.java
@@ -0,0 +1,12 @@
+package teamLeadProjectPKG;
+
+public class LeadClass {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi...QA...!!!");
+		 System.out.println("I am Team Lead...");
+		 System.out.println("This is the code you can refer...!!!");
+	}
+
+}
